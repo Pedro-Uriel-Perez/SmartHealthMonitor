2d22c5cb854ab397637cd6ed6e072fedb9cc8663
Pedro <piter500monzon@gmail.com>
2026-05-26 17:46:12 -0600

    feat: DashboardScreen + NavGraph + TarjetaDato S5

--- app/src/main/java/mx/edu/utng/smarthealthmonitor/MainActivity.kt
+++ app/src/main/java/mx/edu/utng/smarthealthmonitor/MainActivity.kt
@@ -1,27 +1,15 @@
 package mx.edu.utng.smarthealthmonitor
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import mx.edu.utng.smarthealthmonitor.ui.theme.SmartHealthMonitorTheme
+import mx.edu.utng.smarthealthmonitor.navigation.SmartHealthNavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SmartHealthMonitorTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    LoginScreen(
-                        onLoginSuccess = {
-                            Log.d("SmartHealth", "Login exitoso")
-                        }
-                    )
-                }
-            }
+            SmartHealthNavGraph()
         }
     }
 }
