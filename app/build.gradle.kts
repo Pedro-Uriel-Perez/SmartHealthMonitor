b80e648d4507d9912a5ffd3af63ab11c14fcfc8e
Pedro <piter500monzon@gmail.com>
2026-07-11 18:29:10 -0600

    feat(cast): add Cast SDK dependencies to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,8 @@ dependencies {
     implementation("com.google.android.gms:play-services-wearable:18.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
     implementation(project(":data"))
+
+    // Cast SDK
+    implementation("androidx.mediarouter:mediarouter:1.7.0")
+    implementation("com.google.android.gms:play-services-cast-framework:21.5.0")
 }
